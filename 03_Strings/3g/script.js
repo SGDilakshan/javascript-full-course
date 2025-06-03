1ad1cb8b6ccd9333d42f10734137d9d77b35a463
SGDilakshan <sgd.dilakshan@gmail.com>
2025-06-03 13:03:24 +0530

    You order 1 coffee ($5.99) and 1 bagel ($2.95). Using math, calculate the total cost, and using concatenation, create the text: 'Total cost: $____' (hint: calculate in cents to avoid inaccuracies) updated

---
+++ 03_Strings/3g/script.js
@@ -0,0 +1,5 @@
+// 3g) You order 1 coffee ($5.99) and 1 bagel ($2.95). Using math, calculate the total 
+// cost, and using concatenation, create the text:
+// 'Total cost: $____' (hint: calculate in cents to avoid inaccuracies).
+
+console.log('Total cost: $'+ (599+295)/100);
