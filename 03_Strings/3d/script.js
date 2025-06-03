0e0fed10513f234d82fd5a65cdaaf2ef2674bf81
SGDilakshan <sgd.dilakshan@gmail.com>
2025-06-03 12:50:41 +0530

    At arestaurant, you order 1 coffee ($5) and 1 bagel ($3). Uisng math, calculate the total cost, and using concententation, create the text: 'Total cost: $______' (replace _____ with the total you calculated) updated

---
+++ 03_Strings/3d/script.js
@@ -0,0 +1,6 @@
+// 3d) At arestaurant, you order 1 coffee ($5) and 1 bagel ($3). Uisng math, calculate 
+// the total cost, and using concententation, create the text:
+// 'Total cost: $______' (replace _____ with the total you calculated).
+
+
+console.log('Total cost: $' + (5 + 3));
