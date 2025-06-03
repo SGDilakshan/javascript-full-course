54b2e660b2f28a03fa3fbd0f66eae214a9d76edc
SGDilakshan <sgd.dilakshan@gmail.com>
2025-06-03 11:30:18 +0530

    The temperature is -5c. Calculate the temperature in fahrenheit updated

---
+++ 02_Numbers & Math/2n/script.js
@@ -0,0 +1,7 @@
+// We'll use Javascript temperature from celsius to fahrenheit. The formula is:
+//  Fahrenheit = (Celsius * 9/5) + 32 
+//  celsius = (fahrenheit - 32) * 5/9 
+ 
+//  2n) The temperature is -5c. Calculate the temperature in fahrenheit
+
+console.log((-5 * 9/5) + 32);
