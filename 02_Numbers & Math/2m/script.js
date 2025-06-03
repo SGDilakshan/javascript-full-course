04b8d93aa9e17d2f1de764c96d9d85d6688362a1
SGDilakshan <sgd.dilakshan@gmail.com>
2025-06-03 11:27:57 +0530

    The temperature is 86f. Calculate the temperature in celsius updated

---
+++ 02_Numbers & Math/2m/script.js
@@ -0,0 +1,7 @@
+// We'll use Javascript temperature from celsius to fahrenheit. The formula is:
+//  Fahrenheit = (Celsius * 9/5) + 32 
+//  celsius = (fahrenheit - 32) * 5/9 
+ 
+//  2m) The temperature is 86f. Calculate the temperature in celsius
+
+console.log((86 - 32) * 5/9 );
