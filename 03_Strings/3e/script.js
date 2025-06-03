5bda168f8f949d82b68527ed56cc70d1744ff321
SGDilakshan <sgd.dilakshan@gmail.com>
2025-06-03 12:54:47 +0530

    Do the same thing as 3d, but use a template string and interpolation updated

---
+++ 03_Strings/3e/script.js
@@ -0,0 +1,8 @@
+// 3d) At arestaurant, you order 1 coffee ($5) and 1 bagel ($3). Uisng math, calculate 
+// the total cost, and using concententation, create the text:
+// 'Total cost: $______' (replace _____ with the total you calculated).
+
+// 3e) Do the same thing as 3d, but use a template string and interpolation.
+
+
+console.log(`Total cost: $${5+3}`);
