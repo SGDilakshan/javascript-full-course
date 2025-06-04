b3e21e79fd0ab413e9c19aef9086e39c941e8150
SGDilakshan <sgd.dilakshan@gmail.com>
2025-06-04 23:00:05 +0530

    Create second line of text: 'Shopping & handling: $9.98' (use math) updated

---
+++ 03_Strings/3l/script.js
@@ -0,0 +1,5 @@
+// In the amazon project, update the cart to 2 basketballs ($20.95 each) with $4.99 
+// shipping, and 2 t-shirts ($7.99 each) with $4.99 shipping.
+// 3l) Create second line of text: 'Shopping & handling: $9.98' (use math).
+
+console.log(`Shopping & handling: $${(499 + 499) / 100}`);
