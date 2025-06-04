c2ceda877b0d7871ea5eb45da124f7388c842f08
SGDilakshan <sgd.dilakshan@gmail.com>
2025-06-04 23:04:34 +0530

    Create third line: 'Total before tax: $67.68' (use math) updated

---
+++ 03_Strings/3m/script.js
@@ -0,0 +1,5 @@
+// In the amazon project, update the cart to 2 basketballs ($20.95 each) with $4.99
+// shipping, and 2 t-shirts ($7.99 each) with $4.99 shipping.
+// 3m) Create third line: 'Total before tax: $67.68' (use math).
+
+console.log(`Total before tax: $${(2 * 2095 + 2 * 799 + 499 + 499) / 100}`);
